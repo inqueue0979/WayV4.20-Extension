e5c56f4678e7b1b515584120ba838dfb4c060c9c
inqueue0979 <inqueue0979@gmail.com>
2024-10-04 02:29:35 +0900

    Changed URL w.Close  Beta Testing

--- public/background.js
+++ public/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason === "install") {
       chrome.tabs.create({
-        url: "https://wayv.vercel.app" // 확장프로그램 설치시 열 URL (추후 설문조사로 변경 필요)
+        url: "https://docs.google.com/forms/d/17CT0E3JICiOOKpeS_nGHDqiLcK2g1MS0tfOnC9sJBRE/edit" // 확장프로그램 설치시 열 URL (추후 설문조사로 변경 필요)
       });
     }
   });
